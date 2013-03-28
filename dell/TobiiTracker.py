a3e4df14107aaa80351f08d79513ff1a79297f65
Johan Fogelström <zapmew@gmail.com>
2013-03-28 12:09:18 +0100

    Most of an naive implementation in place.
    Now it is only the rest left.
    And fixings.
    And testing.
    And debugging.
    And that stuff that happens but no one thinks about beforehand.
    I hate that stuff.

--- dell/TobiiTracker.py
+++ dell/TobiiTracker.py
@@ -1,7 +1,10 @@
 from multiprocessing import Process
 from threading import (Thread, Lock)
-from ETinterface import (EyeTracker, ETEvent, ETError)
 import time
+import Queue
+
+from ETinterface import (EyeTracker, ETEvent, ETError)
+
 from tobii import eye_tracking_io as etio
 from etio import (browsing, mainloop, )
 
@@ -11,22 +14,53 @@ class AnalyticsTracker(EyeTracker):
     def __init__(self, onETEvent, name=1, fps=60):
        etio.init()
        self.running = True
+       self.queue = Queue.Queue()
 
        self.mainloop = etio.mainloop.MainloopThread()
        
-       self.thread = Thread(target=self.run)
-       self.thread.start()       
+       self.thread = Thread(target=self.connect)
+       self.thread.start()
+
+       def marshaller():
+           for job in self.queue:
+               job()
+       
+       Thread(target=marshaller).start()
+       
 
     # Set if the tracker should be active.
     # callback is called with a result, *args and **kwargs when the operation is completed.
     def enable(self, callback, *args, **kwargs):
-        raise NotImplementedError
+        weight = { # These are the values we can get.
+            (0,0): (0.5, 0.5) #(left_validity, right_validity): (left_weight, right_weight)
+            (4,0): (0.1, 0.9)
+            (3,1): (0.3, 0.7)
+            (2,2): (0.5, 0.5)
+            (1,3): (0.7, 0.3)
+            (0,4): (0.9, 0.1)
+            (4,4): (0, 0)
+            }
+        def henshin(gdi):
+            lx, ly, lv = gdi.LeftGazePoint2D, gdi.LeftValidity
+            rx, ry, lv = gdi.RightGazePoint2D, gdi.LeftValidity
+            timestamp = gdi.Timestamp
+
+            lweight, rweight = weights.get((lv, rv), (0,0))
+
+            x = lx*lweight + rx*rweight
+            y = lxy*lweight + ry*rweight
+
+            etevent = ETEvent(x, y, timestamp)
+            self.callETEvent(etevent)
+            
+        self.et.StartTracking(henshin)
+        callback(True, *args, **kwargs)
 
     # Set if the tracker should be active or not.
     # This might shutdown the tracker if the implementation wants to.
     # callback is called with a result, *args and **kwargs when the operation is completed.
     def disable(self, callback, *args, **kwargs):
-        raise NotImplementedError
+        self.et.StopTracking(callback, *args, **kwargs)
 
     # Allows callers to query the tracker for a statuscode.
     # Specific to implementations but some codes should be respected
@@ -47,20 +81,35 @@ class AnalyticsTracker(EyeTracker):
     # If the tracker could not be placed in calibration mode, return False.
     # callback is called with a result, *args and **kwargs when the operation is completed.
     def startCalibration(self, angle, callback, *args, **kwargs):
-        raise NotImplementedError
+        if self.et == None:
+            callback(False, *args, **kwargs)
+            return
+        
+        #self.et.StopTracking()
+        self.et.StartCalibration(self, callback, True, *args, **kwargs) 
 
     # Takes the tracker out of calibration mode.
     # If the tracker could not be taken out of calibration mode, return False
     # callback is called with a result, *args and **kwargs when the operation is completed.
     def endCalibration(self, callback, *args, **kwargs):
-        raise NotImplementedError
+        if self.et == None:
+            callback(False, *args, **kwargs)
+            return
+
+        def onComputationCompleted():
+            self.enqueue(self.et.StopCalibration,callback, True, *args, **kwargs)
+
+        self.et.ComputeCalibration(onComputationCompleted)
 
     # Clears any calibration actions done.
     # Restore the tracker to a state equal to that
     # right after calibration was initiated.
     # callback is called with *args and **kwargs when the operation is completed.
     def clearCalibration(self, callback, *args, **kwargs):
-        raise NotImplementedError
+        if self.et == None:
+            callback(False, *args, **kwargs)
+            return
+        self.et.ClearCalibration(callback, True, *args, **kwargs)
 
     # Adds the point (x, y) to the calibration.
     # When this is called, the user is expected to be looking at that point.
@@ -69,7 +118,10 @@ class AnalyticsTracker(EyeTracker):
     # result is False if the tracker is not calibrating.
     # Otherwise, result is True.
     def addPoint(self, x, y, callback, *args, **kwargs):
-        raise NotImplementedError
+        if self.et == None or not self.calibrating:
+            callback(False, *args, **kwargs)
+            return
+        self.et.AddCalibrationPoint(self, (x, y), callback=None, True, *args, **kwargs)
 
     # Free for interpretation of the implementor.
     # callback is called with name, *args and **kwargs when the operation is completed.
@@ -82,7 +134,10 @@ class AnalyticsTracker(EyeTracker):
     # If -1 is returned, the implementation takes it upon itself to be able to handle all requested framerates.
     # callback is called with rates, *args and **kwargs when the operation is completed.
     def getRates(self, callback, *args, **kwargs):
-        callback(set([-1]), *args, **kwargs)
+        if self.et == None:
+            callback(set([0]), *args, **kwargs)
+            return
+        self.et.EnumerateFramerates(callback, *args, **kwargs)
 
     def getRate(self, callback, *args, **kwargs):
         callback(0, *args, **kwargs)
@@ -92,24 +147,26 @@ class AnalyticsTracker(EyeTracker):
     # callback is called with result, *args and **kwargs when the operation is completed.
     # result is the rate that is set.
     def setRate(self, rate, callback, *args, **kwargs):
-        raise NotImplementedError
+        if self.et == None:
+            callback(False, *args, **kwargs)
+            return
+        self.et.SetFramerate(rate, callback, *args, **kwargs)
 
-    def run(self):
-
-        lock = threading.Lock()
-
-        #Does this work? YES IT DOES!
-        def etlooker(event_type, event_name, eyetracker_info):
-            pass
+    def etlooker(self):
         
-        lock.acquire() #asynchronous to synchronous
+
+    def connect(self):
         browser = etio.browsing.EyetrackerBrowser(self.mainloop, etlooker)
-        lock.acquire()
-        lock.release()
         
         while self.running:
             pass
 
         browser.stop()
         self.mainloop.stop()
+
+    def enqueue(self, callback, *args, **kwargs):
+        def f():
+            callback(*args, **kwargs)
+            
+        self.queue.put(f)
     
