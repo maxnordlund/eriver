91b78f0fd7b891c095068ec8bcfdd84976b7a4ff
Andreas Broström <andreas.brostrom@gmail.com>
2013-03-29 13:07:32 +0100

    Added a prestart script that appends node_modules to your NODE_PATH

--- node/prestart.sh
+++ node/prestart.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
-# TODO export NODE_PATH and add this directory to it (ask Max)
-# TODO add this to prestart in package.json
+#!/bin/bash
+current_dir=$(pwd)
+script_dir=$(dirname $0)
+if [ $script_dir != '.' ]
+  then
+  current_dir="$current_dir/$script_dir"
+fi
+#echo "node_path before change = $NODE_PATH"
+node_modules="$current_dir/node_modules"
+export NODE_PATH="$NODE_PATH:$node_modules"
+echo "NODE_PATH = $NODE_PATH"
+#echo "node_path after change = $NODE_PATH"
+exit
