18463018e571401c3a43cd4943e73e8dc008e1dc
Andreas Broström <andreas.brostrom@gmail.com>
2013-04-02 10:34:38 +0200

    This fixes issues #6, #7, #32

--- node/prestart.sh
+++ node/prestart.sh
@@ -5,9 +5,7 @@ if [ $script_dir != '.' ]
   then
   current_dir="$current_dir/$script_dir"
 fi
-#echo "node_path before change = $NODE_PATH"
 node_modules="$current_dir/node_modules"
 export NODE_PATH="$NODE_PATH:$node_modules"
 echo "NODE_PATH = $NODE_PATH"
-#echo "node_path after change = $NODE_PATH"
 exit
