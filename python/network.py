cc77bb4c350c26fff586465a84cc1f823503511e
Max Nordlund <max.nordlund@gmail.com>
2013-04-25 20:07:42 +0200

    Forced to add network.py due to git being git

--- python/network.py
+++ python/network.py
@@ -1,66 +1,31 @@
-from socket import *
+import asyncore, socket
 
-class NetHandler(object):
-     def __init__(self, kind, address=('localhost', 3031), blocking=None, listen=False, sock=None):
-          if (sock == None):
-               self.socket = socket(AF_INET, kind)
-          else:
-               self.socket = sock
-          self.address = address
-          self.set_blocking(blocking)
-          self._listen = listen
-          
+class Network(asyncore.dispatcher_with_send):
+  """This is a TCP handler."""
 
-     def send(self, data, address=None):
-          raise NotImplementedError('You need one of the child classes that implement this method.')
+  def __init__(self, address=('localhost', 3031)):
+    super(Network, self).__init__(self)
+    self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.connect(address)
 
-     def recieve(self, size):
-          raise NotImplementedError('You need one of the child classes that implement this method.')
+  def handle_connect(self):
+    pass
 
-     def close(self):
-          self.socket.close()
+  def handle_close(self):
+    self.close()
 
-     def set_blocking(self, blocking=0):
-          self.blocking = blocking
-          self.socket.settimeout(blocking)
+  def handle_read(self):
+    print self.recv(8192)
 
-     #With statement methods
-     def __enter__(self):
-          if self._listen:
-               self.socket.bind(self.address)
-               self.socket.listen(5)
+  #With statement methods
+  def __enter__(self):
+      if self._listen:
+           self.socket.bind(self.address)
+           self.socket.listen(5)
 
-     def __exit__(self, type, vale, traceback):
-          self.close()
-    
+  def writable(self):
+    return (len(self.buffer) > 0)
 
-class UDPHandler(NetHandler):
-    def __init__(self, address=('localhost', 3031), blocking=None, listen=False, socket=None):
-        super(UDPHandler, self).__init__(SOCK_DRAM, address, blocking, listen, socket)
-        
-    def send(self, data, address=None):
-        if not address == None:
-            self.socket.sendto(data, address)
-        else:
-            self.socket.sendto(data, self.address)
-
-    def recieve(self, size):
-        return self.socket.recvfrom(size)
-            
-
-
-class TCPHandler(NetHandler):
-    def __init__(self, address=('localhost', 3031), blocking=None, listen=False, sock=None):
-        super(TCPHandler, self).__init__(SOCK_STREAM, address, blocking, listen, sock)
-        if not listen and sock == None:
-             self.socket.connect(address)
-           
-    def send(self, data):
-            self.socket.sendall(data)
-
-    def recieve(self, size):
-        return self.socket.recv(size)
-
-    def accept(self):
-        conn, addr = self.socket.accept()
-        return TCPHandler(addr, self.blocking, False, conn), addr
+  def handle_write(self):
+    sent = self.send(self.buffer)
+    self.buffer = self.buffer[sent:]
