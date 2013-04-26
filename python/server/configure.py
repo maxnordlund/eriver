743e338bf6e2d0e78ca8720ccaa2b96d6e9cc299
Max Nordlund <max.nordlund@gmail.com>
2013-04-26 23:14:02 +0200

    Fixed missing path

--- python/server/configure.py
+++ python/server/configure.py
@@ -1,6 +1,7 @@
 import json, copy
 
-ratio_of = lambda string: [int(n) for n in string.split(":")]
+def ratio_of(string):
+  return [int(n) for n in string.split(":")]
 
 def adjust(primary, secondary, subprimary, subsecondary):
   scale = primary / subprimary
@@ -8,37 +9,38 @@ def adjust(primary, secondary, subprimary, subsecondary):
   return normal, (normal - 1) / 2
 
 if __name__ == "__main__":
-  path = "starcraft2.json"
+  in_path  = "starcraft2.json"
+  out_path = "starcraft2.gen.json"
   with open(in_path, encoding="UTF-8") as fil:
     data = json.load(fil)
-    out = copy.deepcopy(data)
-    ratios = data.keys()
-    for ratio in ratios:
-      for name, region in data[ratio]:
-        for subratio in ratios:
-          key = "%s_%s" % ratio, subratio
-          if subratio != ratio:
-            if key not in out:
-              out[key] = dict()
-            subregion = data[subratio][name]
-            width, height = ratio_of(ratio)
-            subwidth, subheight = ratio_of(subratio)
-            if region["type"] == "rect":
-              if width/height > subwidth/subheight:
-                width_scale, delta_width = adjust(height, width, subheight, subwidth)
-                height_scale, delta_height = 1, 0
-              else:
-                height_scale, delta_height = adjust(width, height, subwidth, subheight)
-                width_scale, delta_width = 1, 0
+  out = copy.deepcopy(data)
+  ratios = data.keys()
+  for ratio in ratios:
+    for name, region in data[ratio]:
+      for subratio in ratios:
+        key = "%s_%s" % ratio, subratio
+        if subratio != ratio:
+          if key not in out:
+            out[key] = dict()
+          subregion = data[subratio][name]
+          width, height = ratio_of(ratio)
+          subwidth, subheight = ratio_of(subratio)
+          if region["type"] == "rect":
+            if width/height > subwidth/subheight:
+              width_scale, delta_width = adjust(height, width, subheight, subwidth)
+              height_scale, delta_height = 1, 0
+            else:
+              height_scale, delta_height = adjust(width, height, subwidth, subheight)
+              width_scale, delta_width = 1, 0
 
-              out[key][name] = {
-                "type": "rect"
-                "top": subregion["top"] * height_scale + delta_height,
-                "right": subregion["right"] * width_scale + delta_width,
-                "bottom": subregion["bottom"] * height_scale + delta_height,
-                "left": subregion["left"] * width_scale + delta_width
-              }
-            elif region["type"] == "circle":
-              out[key][name] = copy.deepcopy(subregion)
+            out[key][name] = {
+              "type": "rect"
+              "top": subregion["top"] * height_scale + delta_height,
+              "right": subregion["right"] * width_scale + delta_width,
+              "bottom": subregion["bottom"] * height_scale + delta_height,
+              "left": subregion["left"] * width_scale + delta_width
+            }
+          elif region["type"] == "circle":
+            out[key][name] = copy.deepcopy(subregion)
   with open(out_path, "w", encoding="UTF-8") as fil:
     json.dump(out_path, fil, indent=4, separators=(',', ': '))
