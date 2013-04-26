a36904ade27f7c0d1ad6c56296c851b1a38c9c0a
SudoQ <simj91@gmail.com>
2013-04-26 17:47:21 +0200

    Some changes, commit before removing

--- python/server/Handler.py
+++ python/server/Handler.py
@@ -1,16 +1,19 @@
+import asyncore
+
 class Handler(asyncore.dispatcher_with_send):
   """This is a handler for a single player."""
-  #TODO skriv om eller skriv klart här!
+  #TODO skriv om eller skriv klart har!
   def __init__(self, index, config):
-    super(Handler, self).__init__(self)
+    #super(Handler, self).__init__(self)
+    super(dispatcher,self).__init__(self)
     self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
     self._regions = list()
     self._heatmap = Heatmap(index, config.heatmap)
     # self._database = Database(index)
-    for region in config.areas
-      if region.type == "rect"
+    for region in config.areas:
+      if region.type == "rect":
         self._regions.append(Rectangle(index, region))
-      elif region.type == "circle"
+      elif region.type == "circle":
         self._regions.append(Circle(index, region))
 
   def start(self, address=('localhost', 3031)):
