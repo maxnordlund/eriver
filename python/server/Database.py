5b2a7d7692c476e9bacb9f214e290970bfebac39
Max Nordlund <max.nordlund@gmail.com>
2013-04-27 16:34:32 +0200

    Switched to batch processing of queries

--- python/server/Database.py
+++ python/server/Database.py
@@ -7,12 +7,14 @@ class Database(object):
   _insert = "INSERT INTO data (?, ?, ?)"
 
   def __init__(self, db=":memory"):
+    self._queue   = list()
     self._connect = sqlite3.connect(db)
     self._cursor  = self._connect.cursor()
     self._cursor.execute("CREATE TABLE IF NOT EXISTS data (time INT PRIMARY KEY, x REAL NOT NULL, y REAL NOT NULL)")
 
   def __setitem__(self, key, value):
-    self._cursor.execute(Database._insert, (key,) + value)
+    self._queue.append((key,) + value)
 
   def generate(self):
-    pass
+    self._cursor.executemany(Database._insert, self._queue)
+    self._queue = list()
