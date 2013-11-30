f61588df9ca744b18fae82aa51ac4593d5608b89
Oskar Segersvärd <victorystick@gmail.com>
2013-11-30 12:02:53 +0100

    Added detection for when connection to the server is lost.

--- node/public/script/status.js
+++ node/public/script/status.js
@@ -1,15 +1,13 @@
 $(function(){
-  $("h1")[0].style.color = "grey";
-  $("h1").text("No connection");
+  $("h1").text("No connection").css('color', 'gray');
+
   var socket = io.connect(location.protocol + "//" + location.host + "/status");
 
   window.socket = socket;
 
-  socket.emit("subscribe"); // subscribing to news
-
   socket.on("update", function(data) {
     console.log("update", data);
-    var element, index;
+    var element, index, obj;
     for(index in data) {
       obj = data[index];
       element = $("#" + index);
@@ -17,59 +15,23 @@ $(function(){
       element.find(".info").text(obj.info).css("color", obj.color);
       element.find(".ip").text(obj.ip);
       if(obj.available) {
-        element.removeClass("unavailable");
-        element.attr("href", "/calibrate/" + obj.id + "#");
+        element.removeClass("unavailable").attr("href", "/calibrate/" + obj.id + "#");
       } else {
-        element.addClass("unavailable");
-        element.attr("href", "#");
+        element.addClass("unavailable").attr("href", "#");
       }
     }
-    /*switch(data.what)
-    {
-    case "connecting":
-      break;
-    case "new":
-      element = $("#" + data.id);
-      element.find(".id").text(data.id);
-      element.find(".ip").text(data.ip);
-      element.removeClass("unavailable");
-      element.attr("href", "/calibrate/" + data.id + "#");
-      break;
-    case "calibrating":
-      element = $("#" + data.id);
-      element.find(".info").text("Calibrating").css("color", "maroon");
-      break;
-    case "inuse":
-      element = $("#" + data.id);
-      element.addClass("unavailable");
-      element.attr("href", "#");
-      element.find(".info").text("In use").css("color", "maroon");
-      break;
-    case "done":
-      element = $("#" + data.id);
-      element.removeClass("unavailable");
-      element.attr("href", "/calibrate/" + data.id + "#");
-      //element.find(".info").text("");
-      break;
-    case "calibrated":
-      element = $("#" + data.id);
-      element.find(".info").text("Calibrated").css("color", "green");
-      break;
-    case "disconnect":
-      element = $("#" + data.id);
-      element.find(".id").text("?");
-      element.find(".ip").text("unknown");
-      element.find(".info").text("");
-      element.addClass("unavailable");
-      element.attr("href", "#");
-      break;
-    default:
-      //code to be executed if n is different from case 1 and 2
-    }*/
   });
 
-  socket.on("subscribe", function() {
-    $("h1").text("ET Status");
-    $("h1")[0].style.color = "white";
+  socket.on('connect', function () {
+    $("h1").text("ET Status").css('color', 'white');
+    socket.emit("subscribe"); // subscribing to news
+  });
+
+  socket.on('disconnect', function () {
+    $('h1').text('Reconnecting...').css('color', 'gray');
+
+    var els = $('.etStatus').addClass('unavailable').attr('href', '#');
+    els.find('.info').text('');
+    els.find('.ip').text('unknown');
   });
 });
